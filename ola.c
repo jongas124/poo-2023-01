8696b29590fded8de403030b5ef766aaefbb32b2
jongas124 <kossos124@gmail.com>
2023-05-04 16:41:02 -0300

    arvore binaria

--- ola.c
+++ ola.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+typedef struct Noh{
+    int no;
+    struct Noh *esq;
+    struct Noh *dir;
+}noh;
+
+noh *cria_noh(int nno);
+
+void insere_noh(noh **raiz, int nno);
+
+void imprime_inordem(noh **raiz);
+
 int main(){
     printf("ola\n");
     return 0;
